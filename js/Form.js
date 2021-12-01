0f3e5207d7311e0b9abc5d09f3b3fb82d28b817c
Varnika2707 <87861443+Varnika2707@users.noreply.github.com>
2021-12-01 18:58:51 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -1,21 +1,21 @@
 class Form {
   constructor() {
-    this.input = createInput("").attribute("placeholder", "Enter your name");
+    this.inputText = createInput("").attribute("placeholder", "Enter your name");
     this.playButton = createButton("Play");
-    this.titleImg = createImg("./assets/title.png", "game title");
+    this.titleImg = createImg("assets/title.png", "game title");
     this.greeting = createElement("h2");
   }
 
   setElementsPosition() {
     this.titleImg.position(width/2-100, 50);
-    this.input.position(width / 2 , height / 2 - 80);
-    this.playButton.position(width / 2 - 90, height / 2 - 20);
+    this.inputText.position(width/2 - 110,height/2 - 80);
+    this.playButton.position(width/2 - 110,height/2 - 60);
     this.greeting.position(width / 2 - 300, height / 2 - 100);
   }
 
   setElementsStyle() {
     this.titleImg.class("gameTitle");
-    this.input.class("customInput");
+    //this.input.class("customInput");
     this.playButton.class("customButton");
     this.greeting.class("greeting");
   }
@@ -23,19 +23,19 @@ class Form {
   hide() {
     this.greeting.hide();
     this.playButton.hide();
-    this.input.hide();
+    this.inputText.hide();
   }
 
   handleMousePressed() {
     this.playButton.mousePressed(() => {
-      this.input.hide();
+      this.inputText.hide();
       this.playButton.hide();
       var message = `
-      Hello ${this.input.value()}
+      Hello ${this.inputText.value()}
       </br>wait for another player to join...`;
       this.greeting.html(message);
       playerCount += 1;
-      player.name = this.input.value();
+      player.name = this.inputText.value();
       player.index = playerCount;
       player.addPlayer();
       player.updateCount(playerCount);
@@ -49,3 +49,4 @@ class Form {
     this.handleMousePressed();
   }
 }
+
